df93572b4ae84a7da646dde40df93f4c6bdb89b9
peyotor <pyotor@yahoo.com>
2019-11-04 04:44:24 +0300

    Добавлены Кошельки(Wallets)

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_04_004021) do
+ActiveRecord::Schema.define(version: 2019_11_04_014326) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -42,4 +42,13 @@ ActiveRecord::Schema.define(version: 2019_11_04_004021) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  create_table "wallets", force: :cascade do |t|
+    t.integer "money"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_wallets_on_user_id"
+  end
+
+  add_foreign_key "wallets", "users"
 end
